1a17bf2115acff7673f919787613adf8f3da76f1
Israel Deorce <israel.idvj@gmail.com>
2017-06-12 02:51:10 -0300

    #11 Consulta 1 Implementada
    Está funcionando! Também adicionei comentários nos métodos para
    facilitar o entendimento.
    
    Falta ainda estilizar o JavaFX, mas deixamos isso para depois pois é
    menos importante!

--- MyFlight-base/src/pucrs/myflight/gui/JanelaFX.java
+++ MyFlight-base/src/pucrs/myflight/gui/JanelaFX.java
@@ -40,11 +40,10 @@ public class JanelaFX extends Application {
 	final SwingNode mapkit = new SwingNode();
 
 	private GerenciadorCias gerCias;
-	private GerenciadorPaises gerPaises;	
+	private GerenciadorPaises gerPaises;
 	private GerenciadorAeronaves gerAvioes;
 	private GerenciadorAeroportos gerAero;
-	private GerenciadorRotas gerRotas;	
-
+	private GerenciadorRotas gerRotas;
 
 	private GerenciadorMapa gerenciador;
 
@@ -52,9 +51,11 @@ public class JanelaFX extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+
+		// Carrega os dado=========================================
 		setup();
-		
+
+		// Seta os atributos do JavaFX=============================
 		GeoPosition poa = new GeoPosition(-30.05, -51.18);
 		gerenciador = new GerenciadorMapa(poa, GerenciadorMapa.FonteImagens.VirtualEarth);
 		mouse = new EventosMouse();
@@ -63,47 +64,49 @@ public class JanelaFX extends Application {
 
 		createSwingContent(mapkit);
 
-		BorderPane pane = new BorderPane();			
+		BorderPane pane = new BorderPane();
 		GridPane leftPane = new GridPane();
-		
+
 		leftPane.setAlignment(Pos.CENTER);
 		leftPane.setHgap(10);
 		leftPane.setVgap(10);
-		leftPane.setPadding(new Insets(10,10,10,10));
-		
-		
-		
-		Button btnConsulta = new Button("TESTEEEEEEEEEEEEEEE");			
-		leftPane.add(btnConsulta, 0,0);	
-		/*
-		leftPane.add(new Button("BBBB"), 0,1);
-		leftPane.add(new Button("CCCC"), 0,2);
-		leftPane.add(new Button("DDDD"), 0,3);
-		*/	
+		leftPane.setPadding(new Insets(10, 10, 10, 10));
+		// ==========================================================
+
+		// Teste do Professor========================================
+		Button btnConsulta = new Button("Teste do Professor");
+		leftPane.add(btnConsulta, 0, 0);
 		btnConsulta.setOnAction(e -> {
 			consulta();
 		});
-		
-		
-		//Botoes da Consulta 1
-		Label consultaUmLB = new Label("Cons.1 : Lista Companhias");	
-		
+		// ==========================================================
+
+		// Botoes da Consulta 1======================================
+		Label consultaUmLB = new Label("Cons.1 : Lista Companhias");
 		ComboBox consultaUmCB = new ComboBox();
-		//consultaUmCB.getItems().addAll(gerCias.getLista());
-		
+		consultaUmCB.getItems().addAll(gerCias.listarCiasAereas());
 		Button consultaUmBT = new Button("Exibir");
 		consultaUmBT.setOnAction(e -> {
-			gerenciador.clear();	
-			consultaUm(consultaUmCB);	
+			gerenciador.clear();
+			consultaUm(consultaUmCB);
 			gerenciador.getMapKit().repaint();
 		});
-		
-		leftPane.add(consultaUmLB, 0,1);
+		leftPane.add(consultaUmLB, 0, 1);
 		leftPane.add(consultaUmCB, 0, 2);
 		leftPane.add(consultaUmBT, 0, 3);
-		
-		
-		
+		// ==========================================================
+
+		// Funcao teste que exibe todos os Aeroportos================
+		Label exibeTodosLB = new Label("Mostrar  todos Aeroportos");
+		Button exibeTodosBT = new Button("Exibir");
+		exibeTodosBT.setOnAction(e -> {
+			exibeTodos();
+		});
+		leftPane.add(exibeTodosLB, 0, 4);
+		leftPane.add(exibeTodosBT, 0, 5);
+		// ==========================================================
+
+		// Montando a tela do JavaFX=================================
 		pane.setCenter(mapkit);
 		pane.setLeft(leftPane);
 
@@ -111,123 +114,152 @@ public class JanelaFX extends Application {
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Mapas com JavaFX");
 		primaryStage.show();
+		// ==========================================================
 
 	}
 
-    // Inicializando os dados aqui...
-    private void setup() {
+	// Inicializando os dados aqui...
+	private void setup() {
 
-    	gerCias = new GerenciadorCias();
-    	gerPaises = new GerenciadorPaises();
-    	gerAvioes = new GerenciadorAeronaves();    
-    	gerAero = new GerenciadorAeroportos();
-    	gerRotas = new GerenciadorRotas();
-    	
+		gerCias = new GerenciadorCias();
+		gerPaises = new GerenciadorPaises();
+		gerAvioes = new GerenciadorAeronaves();
+		gerAero = new GerenciadorAeroportos();
+		gerRotas = new GerenciadorRotas();
+
 		try {
 			gerCias.carregaDados();
 		} catch (IOException e) {
 			System.out.println("Impossivel ler airlines.dat!");
-			System.out.println("Msg: "+e);
+			System.out.println("Msg: " + e);
 			System.exit(1);
 		}
-		
+
 		//
 		// Chame aqui a leitura dos demais arquivos
 		//
-		
+
 		try {
 			gerPaises.carregaDados();
 		} catch (IOException e) {
 			System.out.println("Impossivel ler countries.dat!");
-			System.out.println("Msg: "+e);
+			System.out.println("Msg: " + e);
 			System.exit(1);
 		}
-		
+
 		try {
 			gerAvioes.carregaDados();
 		} catch (IOException e) {
 			System.out.println("Impossivel ler equipment.dat!");
-			System.out.println("Msg: "+e);
+			System.out.println("Msg: " + e);
 			System.exit(1);
 		}
-		
+
 		try {
 			gerAero.carregaDados(gerPaises);
 		} catch (IOException e) {
 			System.out.println("Impossivel ler airports.dat!");
-			System.out.println("Msg: "+e);
+			System.out.println("Msg: " + e);
 			System.exit(1);
-		}		
-		
+		}
+
 		try {
 			gerRotas.carregaDados(gerCias, gerAero, gerAvioes);
 		} catch (IOException e) {
 			System.out.println("Impossivel ler routes.dat!");
-			System.out.println("Msg: "+e);
+			System.out.println("Msg: " + e);
 			System.exit(1);
-		}	
+		}
 	}
-    
+
+	/**
+	 * Exemplo de consulta do professor.
+	 */
 	private void consulta() {
+		gerenciador.clear();
 		// Para obter um ponto clicado no mapa, usar como segue:
-    	GeoPosition pos = gerenciador.getPosicao();     
+		GeoPosition pos = gerenciador.getPosicao();
 
-        // Lista para armazenar o resultado da consulta
-        List<MyWaypoint> lstPoints = new ArrayList<>();
-        
-        // Exemplo de uso:
-        
-        Aeroporto poa = gerAero.getHash().get("POA");
-        Aeroporto gru = gerAero.getHash().get("GRU"); 
-        
-        Geo locPoa = poa.getLocal();
-        Geo locGru = gru.getLocal();
-                       
-        lstPoints.add(new MyWaypoint(Color.BLUE, poa.getNome(), locPoa, 10));
-        lstPoints.add(new MyWaypoint(Color.RED, gru.getNome(), locGru, 5));
+		// Lista para armazenar o resultado da consulta
+		List<MyWaypoint> lstPoints = new ArrayList<>();
 
-        // Informa o resultado para o gerenciador
-        gerenciador.setPontos(lstPoints);
-        
-        // Quando for o caso de limpar os traçados...
-        //gerenciador.clear();
-        
-        // Exemplo: criando um traçado
-        Tracado tr = new Tracado();        
-        // Adicionando as mesmas localizações de antes
-        tr.addPonto(locPoa);
-        tr.addPonto(locGru);
-        tr.setWidth(4);       // largura do traçado
-        tr.setCor(Color.RED); // cor do traçado
-        
-        // E adicionando o traçado...
-        gerenciador.addTracado(tr);        
-        gerenciador.getMapKit().repaint();        
+		// Exemplo de uso:
+
+		Aeroporto poa = gerAero.getHash().get("POA");
+		Aeroporto gru = gerAero.getHash().get("GRU");
+
+		Geo locPoa = poa.getLocal();
+		Geo locGru = gru.getLocal();
+
+		lstPoints.add(new MyWaypoint(Color.BLUE, poa.getNome(), locPoa, 10));
+		lstPoints.add(new MyWaypoint(Color.RED, gru.getNome(), locGru, 5));
+
+		// Informa o resultado para o gerenciador
+		gerenciador.setPontos(lstPoints);
+
+		// Quando for o caso de limpar os traçados...
+		// gerenciador.clear();
+
+		// Exemplo: criando um traçado
+		Tracado tr = new Tracado();
+		// Adicionando as mesmas localizações de antes
+		tr.addPonto(locPoa);
+		tr.addPonto(locGru);
+		tr.setWidth(4); // largura do traçado
+		tr.setCor(Color.RED); // cor do traçado
+
+		// E adicionando o traçado...
+		gerenciador.addTracado(tr);
+		gerenciador.getMapKit().repaint();
 	}
-	
-	private void consultaUm(ComboBox consultaUmCB){
-		//Lista de Aeroportos referentes (Pontinhos)
-		Set<MyWaypoint> aeroportos = new HashSet<MyWaypoint>(); 
-		
-		//Pega a Cia Selecionada no ComboBox
-		CiaAerea ciaSelecionada= (CiaAerea)consultaUmCB.getValue();
-		
-		List<Rota> rotas = gerRotas.listarTodas();	
+
+	/**
+	 * Exibe todos os Aeroportos no mapa
+	 */
+	private void exibeTodos() {
+		gerenciador.clear();
+		List<MyWaypoint> pontos = new ArrayList<>();
+		List<Aeroporto> aeroportos = gerAero.listarAeroportos();
+		for (Aeroporto a : aeroportos)
+			pontos.add(new MyWaypoint(Color.RED, a.getNome(), a.getLocal(), 4));
+		gerenciador.setPontos(pontos);
+	}
+
+	/**
+	 * Desenha todos os aeroportos onde uma determinada companhia a�rea opera.
+	 * Mostra tamb�m as rotas envolvidas
+	 * 
+	 * @param consultaUmCB
+	 */
+	private void consultaUm(ComboBox consultaUmCB) {
+		gerenciador.clear();
+
+		// Lista de Aeroportos referentes (Pontinhos)
+		List<MyWaypoint> aeroportos = new ArrayList<MyWaypoint>();
+
+		// Pega a Cia Selecionada no ComboBox
+		CiaAerea ciaSelecionada = (CiaAerea) consultaUmCB.getValue();
+
+		List<Rota> rotas = gerRotas.listarTodas();
 		Set<Rota> setRotas = new HashSet<>();
-		rotas.stream()
-			.filter(e->e.getCia().equals(ciaSelecionada))
-			.forEach(e -> setRotas.add(e));			
-		
+		rotas.stream().filter(e -> e.getCia().equals(ciaSelecionada)).forEach(e -> setRotas.add(e));
+
 		Set<Aeroporto> setAeros = new HashSet<>();
-		
+
 		setRotas.forEach(e -> {
 			setAeros.add(e.getOrigem());
 			setAeros.add(e.getDestino());
+			Tracado tr = new Tracado();
+			tr.setCor(Color.GREEN);
+			tr.addPonto(e.getOrigem().getLocal());
+			tr.addPonto(e.getDestino().getLocal());
+			gerenciador.addTracado(tr);
 		});
-		
+
 		setAeros.forEach(e -> {
-			aeroportos.add(new MyWaypoint(Color.BLUE, e.getNome(), e.getLocal(), 5));
-		});				
+			aeroportos.add(new MyWaypoint(Color.BLUE, e.getNome(), e.getLocal(), 4));
+		});
+		gerenciador.setPontos(aeroportos);
 	}
 
 	private class EventosMouse extends MouseAdapter {
@@ -241,12 +273,12 @@ public class JanelaFX extends Application {
 			lastButton = e.getButton();
 			// Botão 3: seleciona localização
 			if (lastButton == MouseEvent.BUTTON3) {
-				gerenciador.setPosicao(loc);				
+				gerenciador.setPosicao(loc);
 				gerenciador.getMapKit().repaint();
 			}
 		}
 	}
-	
+
 	private void createSwingContent(final SwingNode swingNode) {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
@@ -255,8 +287,8 @@ public class JanelaFX extends Application {
 			}
 		});
 	}
-	
+
 	public static void main(String[] args) {
-		launch(args);			
+		launch(args);
 	}
 }
